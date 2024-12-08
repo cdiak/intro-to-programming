7ab6d2855a6d3c4e027f51944ec37a1712d13085
Christopher Diak <christopherjdiak@gmail.com>
2024-12-07 23:30:41 -0500

    finished substitution model

---
+++ SICP/Chapter-1-Building-Abstractions-with-Procedures/1.1-The-Elements-of-Programming/1.1.5-The-Substitution-Model-for-Procedure-Application/{code,exercises,notes}/notes.scm
@@ -0,0 +1,13 @@
+Learn your lines:
+
+To apply a compound procedure to arguments,
+evaluate the body of the procedure with each formal
+parameter replaced by the corresponding argument.
+
+This is a substitution MODEL for the behavior of the interpreter.
+It is NOT how the interpreter implements the behavior.
+
+Applicative v. Normal Order Evaluation
+- Normal order: fully expand the combinations, then reduce.
+- Applicative order: avoids multiple evaluations of the same expression
+
