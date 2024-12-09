efe340a93e55fd66e6e43a9d6887bb73a618b4a3
Christopher Diak <christopherjdiak@gmail.com>
2024-12-08 20:59:21 -0500

    added Substitution Model code

---
+++ SICP/Chapter-1-Building-Abstractions-with-Procedures/1.1-The-Elements-of-Programming/1.1.5-The-Substitution-Model-for-Procedure-Application/{code,exercises,notes}/code.scm
@@ -0,0 +1,25 @@
+> (define (square x) (* x x))
+> square 10
+#<procedure square>
+> (square 10)
+100
+> (define (sum-of-squares x y)
+    (+ (square x)(square y)))
+> (sum-of-squares 10 15)
+325
+> (define (f a)
+    (sum-of-squares (+ a 1) (* a )))
+> (f 5)
+61
+> (define (f a)
+    (sum-of-squares (+ a 1) (* a 2)))
+> (f 5)
+136
+> (sum-of-squares (+ 5 1)(* 5 2))
+136
+> (+ (square (+ 5 1))
+     (square (* 5 2)))
+136
+> (+ (* (+ 5 1) (+ 5 1))
+     (* (* 5 2) (* 5 2)))
+136
