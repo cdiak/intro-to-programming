f8a8073a821475aabe2dbfd559dea869fedd611e
Christopher Diak <christopherjdiak@gmail.com>
2024-12-11 22:24:38 -0500

    did 1.2.16

--- Programming in Java/01_Elements_of_Programming/1.2_Built-in_Types_of_Data/Exercises/oneTwoSixteen.java
+++ Programming in Java/01_Elements_of_Programming/1.2_Built-in_Types_of_Data/Exercises/oneTwoSixteen.java
@@ -4,8 +4,11 @@ public class oneTwoSixteen {
         double mass1 = Double.parseDouble(args[0]);
         double mass2 = Double.parseDouble(args[1]);
         double r = Double.parseDouble(args[2]);
-        double force = (G * mass1 * mass2)/r*r;
+        double force = (G * mass1 * mass2)/(r*r);
         System.out.println(force);
     }
 }
 
+// the problem with the original code was that it lacked 
+// parentheses around r*r, so the code was = G*mass1*mass2
+// irrespective of distance.
