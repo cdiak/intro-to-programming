7f3eb6fbe49548381290a1567f51f7f8dd00d7bb
Christopher Diak <christopherjdiak@gmail.com>
2024-12-14 11:22:29 -0500

    finished exercise 1.3

---
+++ SICP/Chapter-1-Building-Abstractions-with-Procedures/1.1-The-Elements-of-Programming/1.1.6-Conditional-Expressions-and-Predicates/Exercise1.3.scm
@@ -0,0 +1,25 @@
+; Exercise 1.3: Define a procedure that takes three numbers as arguments 
+; and returns the sum of the squares of the two larger numbers.
+
+(define (square x) (* x x))
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(define (largest x y z)
+(cond 
+  ((and (> x y) (> x z)) x)  ; x is the largest
+  ((and (> y x) (> y z)) y)  ; y is the largest
+  (else z)))                 ; z is the largest
+
+(define (middle x y z)
+(cond 
+  ((and (> y x) (> x z)) x)  ; x is the middle, y is largest
+  ((and (> z x) (> y z)) x)  ; x is the middle, z is largest
+  ((and (> x y) (> y z)) y)  ; y is the middle, x is largest
+  ((and (> z y) (> y x)) y)  ; y is the middle, z is largest
+  (else z)))                 ; z is the middle(top-two 5 1 3)
+
+(define a largest)
+(define b middle)
+
+(define (top-two x y z) (sum-of-squares (largest x y z)(middle x y z)))
