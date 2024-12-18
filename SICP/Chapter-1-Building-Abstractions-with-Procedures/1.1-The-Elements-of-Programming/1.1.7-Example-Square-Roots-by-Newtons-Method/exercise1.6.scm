046bd0bf85fac979ad5050b873e8cba427cddda2
Christopher Diak <christopherjdiak@gmail.com>
2024-12-17 22:47:34 -0500

    did exercise 1.6

--- SICP/Chapter-1-Building-Abstractions-with-Procedures/1.1-The-Elements-of-Programming/1.1.7-Example-Square-Roots-by-Newtons-Method/exercise1.6.scm
+++ SICP/Chapter-1-Building-Abstractions-with-Procedures/1.1-The-Elements-of-Programming/1.1.7-Example-Square-Roots-by-Newtons-Method/exercise1.6.scm
@@ -17,3 +17,17 @@
           (sqrt-iter (improve guess x) x)))
 
 ; What happens when Alyssa attempts to use this to compute square roots? Explain.
+
+; Explanation: The most important difference between a special form and a procedure
+; is the way that the interpreter handles its evaluation rules. For the special form "if"
+; the evaluation rule is NOT what is defined in 1.1.3 where we first evaluate all of the 
+; subexpressions and then apply the procedure that is the value of the leftmost subexpression 
+; to the arguments that are the values of the other subexpressions.
+
+; new-if is a procedure and hence all the subexpressions are evaluated before new-if is applied
+; to the subexpressions, thus, the recursive function (guess(sqrt-iter(improve guess x)x)) means
+; the eval step goes in an infinite loop at the else-clause, and we're never able to evaluate 
+; the predicate.
+
+; This is different from if, where the interpreter only interprets one of the branches, depending
+; on whether it is true or false.
